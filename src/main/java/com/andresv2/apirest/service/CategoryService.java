b7ba465fdfd97a995501042dcc5670d1a04f2362
Andres Vargas <wanesvargas@gmail.com>
2024-02-04 22:19:57 -0600

    Feature[Category Features] Category  Features
    - Add search for Category and Product
    - Add Update for Category and Product
    - Fix bug on SearchUtils on between Search, now can accept double, String and Integer
    - Add column definition on Boolean flags on Product

--- src/main/java/com/andresv2/apirest/service/CategoryService.java
+++ src/main/java/com/andresv2/apirest/service/CategoryService.java
@@ -4,12 +4,13 @@ import com.andresv2.apirest.entities.Category;
 import com.andresv2.apirest.repository.CategoryRepository;
 import com.andresv2.apirest.repository.ProductRepository;
 import com.andresv2.apirest.util.SearchUtils;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -26,6 +27,17 @@ public class CategoryService {
         return categoryRepo.save(category);
     }
 
+    public Category updateCategory(Long id, HashMap<String, Object> userData) {
+        Category category = categoryRepo.findById(id).orElseThrow(() -> {throw new UsernameNotFoundException("User with ID " + id + " not found");});
+        userData.forEach((key, value) -> {
+            switch (key) {
+                case "name" -> category.setName((String) value);
+                case "description" -> category.setDescription((String) value);
+            }
+        });
+        return categoryRepo.save(category);
+    }
+
     public Category findCategoryById(Long id) {
         Category category = categoryRepo.findById(id).orElseThrow(() -> new RuntimeException("Store not found"));
 
@@ -35,23 +47,6 @@ public class CategoryService {
 
     public Page<Category> findAllCategories(Pageable pageable) {
         return categoryRepo.findAll(pageable);
-    }
-
-    public Page<Category> findAllFilteredCategories(Pageable pageable, JSONObject data) {
-        JSONObject filters = new JSONObject();
-        JSONObject equalsTo = new JSONObject();
-//        JSONObject between = new JSONObject(); //only if filters have between option
-        // Define Filters
-        if (data.has("name")) equalsTo.put("name", data.getString("name"));
-        if (data.has("description")) equalsTo.put("description", data.getString("description"));
-
-        if(equalsTo.length() > 0) filters.put("equals", equalsTo);
-//        if(between.length() > 0) filters.put("between", between); //only if filters have between option
-
-        // if the method have more filters like between or  greater than etc. we need to add the summary of sizes to size key Example size-> filters.put("size", equalsTo.length() + greaterThan.length() + between.length());
-        filters.put("size", equalsTo.length()); // + between.length()
-
-        return categoryRepo.findAll(searchUtilsCategory.getQueryParameters(filters), pageable);
     }
 
     public List<Category> getCategoriesByStoreId(Long storeId) {
--- src/main/java/com/andresv2/apirest/service/ProductService.java
+++ src/main/java/com/andresv2/apirest/service/ProductService.java
@@ -3,11 +3,15 @@ package com.andresv2.apirest.service;
 import com.andresv2.apirest.entities.Product;
 import com.andresv2.apirest.repository.ProductRepository;
 import com.andresv2.apirest.util.SearchUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 public class ProductService {
@@ -21,8 +25,26 @@ public class ProductService {
         return productRepo.save(product);
     }
 
+    public Product updateProduct(Long id, HashMap<String, Object> userData) {
+        Product product = productRepo.findById(id).orElseThrow(() -> {throw new UsernameNotFoundException("User with ID " + id + " not found");});
+        userData.forEach((key, value) -> {
+            switch (key) {
+                case "name" -> product.setName((String) value);
+                case "description" -> product.setDescription((String) value);
+                case "price" -> product.setPrice((Double) value);
+                case "active" -> product.setActive((Boolean) value);
+                case "discount" -> product.setDiscount((Boolean) value);
+                case "stock" -> product.setStock((Integer) value);
+                case "image" -> product.setImage((String) value);
+                case "category" -> product.setCategory((String) value);
+                case "genre" -> product.setGenre((String) value);
+            }
+        });
+        return productRepo.save(product);
+    }
+
     public Product findProductById(Long id) {
-        return productRepo.findById(id).get();
+        return productRepo.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
     }
 
     public Page<Product> findAllProducts(Pageable pageable) {
@@ -32,18 +54,33 @@ public class ProductService {
     public Page<Product> findAllFilteredProducts(Pageable pageable, JSONObject data) {
         JSONObject filters = new JSONObject();
         JSONObject equalsTo = new JSONObject();
-//        JSONObject between = new JSONObject(); //only if filters have between option
+        JSONObject betweenTo = new JSONObject(); //only if filters have between option
         // Define Filters
         if (data.has("name")) equalsTo.put("name", data.getString("name"));
         if (data.has("description")) equalsTo.put("description", data.getString("description"));
-        if (data.has("ramo")) equalsTo.put("ramo", data.getString("ramo"));
         if (data.has("category")) equalsTo.put("category", data.getString("category"));
+        if (data.has("genre")) equalsTo.put("genre", data.getString("genre"));
+        if (data.has("discount")) equalsTo.put("discount", data.getBoolean("discount"));
+        if (data.has("active")) equalsTo.put("active", data.getBoolean("active"));
+
+        if (data.has("price"))
+            if (data.get("price") instanceof JSONArray)
+                betweenTo.put("price", data.getJSONArray("price").toList());
+            else
+                equalsTo.put("price", data.get("price").toString());
+
+        if (data.has("stock"))
+            if (data.get("stock") instanceof JSONArray)
+                betweenTo.put("stock", data.getJSONArray("stock").toList());
+            else
+                equalsTo.put("stock", data.get("stock").toString());
+
 
         if(equalsTo.length() > 0) filters.put("equals", equalsTo);
-//        if(between.length() > 0) filters.put("between", between); //only if filters have between option
+        if(betweenTo.length() > 0) filters.put("between", betweenTo); //only if filters have between option
 
         // if the method have more filters like between or  greater than etc. we need to add the summary of sizes to size key Example size-> filters.put("size", equalsTo.length() + greaterThan.length() + between.length());
-        filters.put("size", equalsTo.length()); // + between.length()
+        filters.put("size", equalsTo.length() + betweenTo.length()); //
 
         return productRepo.findAll(searchUtilsProduct.getQueryParameters(filters), pageable);
     }
