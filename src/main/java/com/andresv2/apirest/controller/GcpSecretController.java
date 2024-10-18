d71cda360530088beaa3362250b8ab90b8a4554e
Andres Vargas <wanesvargas@gmail.com>
2024-10-17 23:59:01 -0600

    Feature[General Feature] Secret Manager Endpoints
    - Add the controller for Secret Manager Endpoints
    - Migrate all the methods to its own class on service package

---
+++ src/main/java/com/andresv2/apirest/controller/GcpSecretController.java
@@ -0,0 +1,12 @@
+package com.andresv2.apirest.controller;
+
+import com.andresv2.apirest.service.GcpSecretService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/secretManager")
+public class GcpSecretController {
+
+}
